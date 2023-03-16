11bf0d7b09951fe6f44a027d621a372e3a6e1099
David Currie <davidcurrie2001@yahoo.co.uk>
2023-03-16 10:48:07 +0000

    Remove old species names and update documentation

--- R/data.R
+++ R/data.R
@@ -1,17 +1,3 @@
-#' Species names and codes.
-#'
-#' A data frame containing the FAO code, FAO description, Aphia ID, and
-#' scientific name for a selection of species
-#'
-#' @format A data frame containing the species information
-#' \describe{
-#'   \item{FAOCode}{Three letter FAO code for species}
-#'   \item{FAODescription}{Text description of the FAO code}
-#'   \item{ScientificName}{Scientific name of the species}
-#'   \item{AphiaID}{Aphia ID of the species}
-#' }
-#' @source David Currie
-"speciesNamesAndCodes"
 #' Species WORMS aphiaids and descriptions.
 #'
 #' A data frame containing the WORMS aphiaid, and
--- data-raw/getSpeciesCodes.R
+++
@@ -1,7 +0,0 @@
-library(openxlsx)
-
-# Read spreadsheet with species codes and names and make it available in
-# the package
-speciesFile <- "./data-raw/SpeciesCodes.xlsx"
-speciesNamesAndCodes <- read.xlsx(speciesFile)
-usethis::use_data(speciesNamesAndCodes, overwrite = TRUE)
