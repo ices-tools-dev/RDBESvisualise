865160dd5318c9c84146997eb94f2847ec4d5852
Kasia-MIR <60775640+Kasia-MIR@users.noreply.github.com>
2024-11-06 10:18:10 +0100

    #7 colours 'recipe'

---
+++ data-raw/palette.R
@@ -0,0 +1,54 @@
+#'Function which create palette
+library(RColorBrewer)
+library(ggplot2)
+
+#define number of extend colors on palette n
+cols <- 10
+#define vector of colours on pallete
+n<-49
+#combine palette
+palette1 <- colorRampPalette(brewer.pal(9, "Set1"), interpolate = "spline")(cols)
+palette2 <- colorRampPalette(brewer.pal(10, "Set3"), interpolate = "spline")(cols)
+palette3 <- colorRampPalette(brewer.pal(8, "Set2"), interpolate = "spline")(cols)
+palette4 <- colorRampPalette(brewer.pal(8, "Dark2"), interpolate = "spline")(cols)
+palette5 <- colorRampPalette(brewer.pal(9, "Pastel1"), interpolate = "spline")(cols)
+combined_palette <- c(palette1, palette2, palette3, palette4, palette5)
+
+#barplot(rep(1, length(combined_palette)), col = combined_palette, border = NA, space = 0, main = "Pallete")
+
+# reads RDBES data
+library(RDBEScore)
+RDBESDataObject <- createRDBESDataObject(input = "D:/RCG-RDBES-Overviews/RegionalOverviews/data_RDBES/001_raw/CL Landing RDBES RCG NANASEA Baltic year 2021-2023 2024_05_28.zip")
+
+CL <- RDBESDataObject[["CL"]]
+
+names(combined_palette) <- levels(as.factor(CL$CLvesFlagCou))
+
+#extend vector
+country_extended <- c(levels(factor(CL$CLvesFlagCou)), rep(NA, length(combined_palette)-length(levels(factor(CL$CLvesFlagCou)))))
+
+#palette in data frame
+palette <- data.frame(country = country_extended,
+                      color = combined_palette)
+# save colours
+write.table(palette,file = 'data/colourCountryTab.txt', sep = "\t",row.names = FALSE, col.names = TRUE)
+
+
+#save definiction of colScale
+colScale <- scale_fill_manual(name = "Country",values = combined_palette)
+
+color_info <- paste("scale_fill_manual(name = 'Country', values =list(",
+                    paste(sprintf("'%s' = '%s'", names(combined_palette), combined_palette), collapse = ", "),
+                    "))")
+# save .txt
+writeLines(color_info, con = "data/colorCountryColScale.txt")
+
+# #How to use in your script
+# #1.load colScale from text file
+# color_country <- readLines("data/color_country.txt")
+#
+# #2. convert text from text into an R object
+# colScale <- eval(parse(text = color_country))
+#
+# #3. Create a plot using the loaded colScale
+
